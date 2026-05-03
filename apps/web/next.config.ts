8c89e7f3d34341dacf249338ca4406c411c278d7
quantumCode <quantumcode22@gmail.com>
2026-05-03 16:18:42 +0530

    initial setup

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@dev-connect/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
